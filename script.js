f98003b35468a5c8294a09e67bd72fa9d9d997be
AxelBresa <bresanovichaxel43@gmail.com>
2024-04-03 00:52:26 -0300

    se agrego las img

--- script.js
+++ script.js
@@ -1,21 +1,26 @@
 function traerPokemones(){
-    fetch("https://pokeapi.co/api/v2/pokemon/")
+  fetch("https://pokeapi.co/api/v2/pokemon/")
   .then(res => res.json())
   .then(json => {
-    const results = json.results; 
-    const data = results.map(pokemon => ({
-      id: pokemon.url.split('/').filter(Boolean).pop(),
-      name: pokemon.name
-    }));
-    console.log(data);
-    const lista=document.getElementById("listado_pokemon");
-    // lista.innerHTML = ''; 
-    data.map(pokemon => {
-      lista.innerHTML += `<p>Name: ${pokemon.name}</p>`;
-    });
+      const results = json.results; 
+      const data = results.map(pokemon => ({
+          id: pokemon.url.split('/').filter(Boolean).pop(),
+          name: pokemon.name,
+          imageUrl: `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemon.url.split('/').filter(Boolean).pop()}.png` 
+      }));
+      console.log(data);
+      const lista = document.getElementById("listado_pokemon");
+      lista.innerHTML = ''; // Limpiar contenido anterior
+      data.map(pokemon => {
+          lista.innerHTML += `
+              <div class="pokemon-item">
+                  <p> <img src="${pokemon.imageUrl}" alt="${pokemon.name}"> 
+                  #${pokemon.id} ${pokemon.name}</p>
+              </div>
+          `;
+      });
   })
   .catch(error => {
-    console.error('Error al obtener los datos:', error);
+      console.error('Error al obtener los datos:', error);
   });
 }
-
